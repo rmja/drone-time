91e7607fd43e2868e12b9706f900b7d9ba934acc
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-01-09 07:33:00 +0100

    Rename systick driver to systick-dint

--- src/drv/mod.rs
+++ src/drv/mod.rs
@@ -1,5 +1,9 @@
 #[cfg(feature = "stm32")]
 pub mod stm32;
 
-#[cfg(feature = "systick")]
-pub mod systick;
+#[cfg(feature = "systick-dint")]
+mod systick_dint;
+#[cfg(feature = "systick-dint")]
+pub mod systick {
+    pub use crate::drv::systick_dint::*;
+}
