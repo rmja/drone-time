0b150781c457e3cd8d6989fac693c4412ca717aa
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-01-13 14:57:21 +0100

    Update alarm.rs

--- src/alarm.rs
+++ src/alarm.rs
@@ -1,45 +1,78 @@
-use core::{future::Future, marker::PhantomData, pin::Pin};
+use core::{cell::RefCell, future::Future, marker::PhantomData, pin::Pin, task::{Context, Poll, Waker}};
 
 use crate::{AlarmTimer, Tick, TimeSpan};
-use alloc::{collections::VecDeque, sync::Arc};
+use alloc::{collections::VecDeque, rc::Rc, sync::Arc};
 use drone_core::sync::Mutex;
 use futures::prelude::*;
 
 /// An alarm is backed by a timer and provides infinite timeout capabilites and multiple simultaneously running timeouts.
 pub struct Alarm<Timer: AlarmTimer<T, A>, T: Tick, A> {
     timer: Timer,
-    running: Option<Pin<Box<dyn Future<Output = ()>>>>,
-    subscriptions: Arc<Mutex<VecDeque<Subscription<T>>>>,
+    subscriptions: VecDeque<Arc<Mutex<Box<Subscription<T>>>>>,
+    // state: Arc<Mutex<State<T, A>>>,
+    tick: PhantomData<T>,
     adapter: PhantomData<A>,
 }
 
-struct Subscription<T: Tick> {
+// struct State<'a, T: Tick, A> {
+//     running: Option<Pin<Box<dyn Future<Output = ()>>>>,
+//     adapter: PhantomData<A>,
+// }
+
+pub struct Subscription<T: Tick>  {
     remaining: TimeSpan<T>,
+    waker: Option<Waker>,
 }
 
-struct SubscriptionFuture;
+pub struct SubscriptionGuard<T: Tick> {
+    // subscriptions: Option<&'a RefCell<VecDeque<Arc<Mutex<Box<Subscription<T>>>>>>>,
+    inner: Arc<Mutex<Box<Subscription<T>>>>,
+    tick: PhantomData<T>,
+}
 
-impl Future for SubscriptionFuture {
+impl<T: Tick + 'static> Future for SubscriptionGuard<T> {
     type Output = ();
 
     fn poll(
         self: Pin<&mut Self>,
-        cx: &mut core::task::Context<'_>,
-    ) -> core::task::Poll<Self::Output> {
-        todo!()
+        cx: &mut Context<'_>,
+    ) -> Poll<Self::Output> {
+        let mut inner = self.inner.try_lock().unwrap();
+        if inner.remaining.0 == 0 {
+            // Timeout has already occured.
+            Poll::Ready(())
+        }
+        else {
+            // Copy the waker to the subscription so that we can wake it when it is time.
+            inner.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
     }
 }
 
-impl<Timer: AlarmTimer<T, A>, T: Tick, A: Send> Alarm<Timer, T, A> {
+impl<T: Tick> Drop for SubscriptionGuard<T> {
+    fn drop(&mut self) {
+        // let subs = self.inner.subscriptions.unwrap().borrow_mut();
+        // let index = subs.into_iter().position(|x| core::ptr::eq(x, self)).unwrap();
+        // subs.remove(index);
+    }
+}
+
+impl<Timer: AlarmTimer<T, A>, T: Tick + 'static, A: Send> Alarm<Timer, T, A> {
     pub const MAX: u32 = Timer::MAX;
 
     /// Create a new `Alarm` backed by a hardware timer.
     pub fn new(timer: Timer) -> Self {
         Self {
             timer,
-            running: None,
-            subscriptions: Arc::new(Mutex::new(VecDeque::new())),
+            subscriptions: VecDeque::new(),
+            tick: PhantomData,
             adapter: PhantomData,
+            // state: Arc::new(Mutex::new(State {
+            //     running: None,
+            //     subscriptions: VecDeque::new(),
+            //     adapter: PhantomData,
+            // }))
         }
     }
 
@@ -55,26 +88,33 @@ impl<Timer: AlarmTimer<T, A>, T: Tick, A: Send> Alarm<Timer, T, A> {
 
     /// Get a future that completes after a delay of length `duration` relative to the counter value `base`.
     pub fn sleep_from(&mut self, base: u32, duration: TimeSpan<T>) -> impl Future<Output = ()> {
-        let sub = Subscription {
+        let index = 2;
+        let sub = Arc::new(Mutex::new(Box::new(Subscription {
             remaining: duration,
-        };
-        
-        let mut subscriptions = self.subscriptions.try_lock().unwrap();
-        let index = Self::get_insert_index(&subscriptions, duration);
-        subscriptions.insert(index, sub);
-        drop(subscriptions);
+            waker: None,
+        })));
 
-        if index == 0 {
-            self.set_running(base, duration);
+        let index = Self::get_insert_index(&self.subscriptions, duration);
+        self.subscriptions.insert(index, sub.clone());
+
+        // let subs = self.subscriptions;
+        // sub.subscriptions = Some(&subs);
+
+        // if index == 0 {
+        //     self.set_running(base, duration);
+        // }
+
+        SubscriptionGuard {
+            // subscriptions: Some(&self.subscriptions),
+            inner: sub,
+            tick: PhantomData,
         }
-
-        SubscriptionFuture
     }
 
-    fn get_insert_index(subs: &VecDeque<Subscription<T>>, remaining: TimeSpan<T>) -> usize {
+    fn get_insert_index(subscriptions: &VecDeque<Arc<Mutex<Box<Subscription<T>>>>>, remaining: TimeSpan<T>) -> usize {
         let mut index = 0;
-        for sub in subs {
-            if remaining < sub.remaining {
+        for sub in subscriptions.iter() {
+            if remaining < sub.try_lock().unwrap().remaining {
                 break;
             }
             index += 1;
@@ -82,38 +122,33 @@ impl<Timer: AlarmTimer<T, A>, T: Tick, A: Send> Alarm<Timer, T, A> {
         index
     }
 
-    fn set_running(&mut self, base: u32, duration: TimeSpan<T>) {
-        let subs = self.subscriptions.clone();
-        let future = self.sleep_timer(base, duration).then(move |f| {
-            let mut subs = subs.try_lock().unwrap();
-            // Set the remaining time for each subscription.
-            for s in subs.iter_mut() {
-                s.remaining -= duration;
-            }
+    // fn set_running(&mut self, base: u32, duration: TimeSpan<T>) {
+    //     let state = self.state.clone();
+    //     let future = self.sleep_timer(base, duration).then(move |f| {
+    //         let mut state = state.try_lock().unwrap();
+    //         // Set the remaining time for each subscription.
+    //         for s in state.subscriptions.iter_mut() {
+    //             s.remaining -= duration;
 
-            // Wake all futures for subscriptions with remaining == 0.
-            while !subs.is_empty() {
-                let s = subs.front().unwrap();
-                if s.remaining.0 == 0 {
-                    subs.pop_front();
+    //             if s.remaining.0 == 0 {
+    //                 state.subscriptions.pop_front();
 
-                    // Wake the future for the subscription.
-                } else {
-                    break;
-                }
-            }
+    //                 // Wake the future for the subscription.
+    //                 s.waker.unwrap().wake();
+    //             }
+    //         }
 
-            let next = subs.front();
-            if let Some(next) = next {
-                let base = Self::counter_add(base, (duration.0 % Timer::PERIOD) as u32);
-                // self.set_running(base, next.remaining);
-            }
+    //         let next = state.subscriptions.front();
+    //         if let Some(next) = next {
+    //             let base = Self::counter_add(base, (duration.0 % Timer::PERIOD) as u32);
+    //             // self.set_running(base, next.remaining);
+    //         }
 
-            future::ready(())
-        });
+    //         future::ready(())
+    //     });
 
-        // self.running = Some(Box::pin(future));
-    }
+    //     // self.running = Some(Box::pin(future));
+    // }
 
     fn counter_add(base: u32, duration: u32) -> u32 {
         assert!(base <= Timer::MAX);
