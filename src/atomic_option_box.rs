fb2c0f9a97144818c705920d0a10ee000730df46
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-01-14 11:55:27 +0100

    Add nostd variants of atomicbox

--- src/atomic_option_box.rs
+++ src/atomic_option_box.rs
@@ -1,90 +1,343 @@
-//! Atomic version of `Option<Box<T>>` inspired by https://github.com/jorendorff/atomicbox
+use core::fmt::{self, Debug, Formatter};
+use core::mem::forget;
+use core::ptr::{self, null_mut};
+use core::sync::atomic::{AtomicPtr, Ordering};
 
-use core::{
-    ptr,
-    sync::atomic::{AtomicPtr, Ordering},
-};
-
-/// An atomic `Option<Box<T>>`.
+/// A type that holds a single `Option<Box<T>>` value and can be safely shared
+/// between threads.
 pub struct AtomicOptionBox<T> {
-    inner: AtomicPtr<T>,
+    /// Pointer to a `T` value in the heap, representing `Some(t)`;
+    /// or a null pointer for `None`.
+    ptr: AtomicPtr<T>,
+}
+
+fn into_ptr<T>(value: Option<Box<T>>) -> *mut T {
+    match value {
+        Some(box_value) => Box::into_raw(box_value),
+        None => null_mut(),
+    }
+}
+
+unsafe fn from_ptr<T>(ptr: *mut T) -> Option<Box<T>> {
+    if ptr.is_null() {
+        None
+    } else {
+        Some(Box::from_raw(ptr))
+    }
 }
 
 impl<T> AtomicOptionBox<T> {
-    /// Create a new `AtomicOptionBox` with a given value.
-    pub fn new(option: Option<Box<T>>) -> AtomicOptionBox<T> {
-        let ptr = match option {
-            Some(data) => Box::into_raw(data),
-            None => ptr::null_mut(),
+    /// Creates a new `AtomicOptionBox` with the given value.
+    ///
+    /// # Examples
+    ///
+    ///     use atomicbox::AtomicOptionBox;
+    ///
+    ///     let atomic_box = AtomicOptionBox::new(Some(Box::new(0)));
+    ///
+    pub fn new(value: Option<Box<T>>) -> AtomicOptionBox<T> {
+        let abox = AtomicOptionBox {
+            ptr: AtomicPtr::new(null_mut()),
         };
-        AtomicOptionBox {
-            inner: AtomicPtr::new(ptr),
+        if let Some(box_value) = value {
+            abox.ptr.store(Box::into_raw(box_value), Ordering::Release);
+        }
+        abox
+    }
+
+    /// Atomically set this `AtomicOptionBox` to `other` and return the
+    /// previous value.
+    ///
+    /// This does not allocate or free memory, and it neither clones nor drops
+    /// any values.  `other` is moved into `self`; the value previously in
+    /// `self` is returned.
+    ///
+    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
+    /// as other values would not be safe if `T` contains any data.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ordering` is not one of the two allowed values.
+    ///
+    /// # Examples
+    ///
+    ///     use core::sync::atomic::Ordering;
+    ///     use atomicbox::AtomicOptionBox;
+    ///
+    ///     let atom = AtomicOptionBox::new(None);
+    ///     let prev_value = atom.swap(Some(Box::new("ok")), Ordering::AcqRel);
+    ///     assert_eq!(prev_value, None);
+    ///
+    pub fn swap(&self, other: Option<Box<T>>, order: Ordering) -> Option<Box<T>> {
+        let mut result = other;
+        self.swap_mut(&mut result, order);
+        result
+    }
+
+    /// Atomically set this `AtomicOptionBox` to `other` and drop the
+    /// previous value.
+    ///
+    /// The `AtomicOptionBox` takes ownership of `other`.
+    ///
+    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
+    /// as other values would not be safe if `T` contains any data.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ordering` is not one of the two allowed values.
+    ///
+    /// # Examples
+    ///
+    ///     use core::sync::atomic::Ordering;
+    ///     use atomicbox::AtomicOptionBox;
+    ///
+    ///     let atom = AtomicOptionBox::new(None);
+    ///     atom.store(Some(Box::new("ok")), Ordering::AcqRel);
+    ///     assert_eq!(atom.into_inner(), Some(Box::new("ok")));
+    ///
+    pub fn store(&self, other: Option<Box<T>>, order: Ordering) {
+        let mut result = other;
+        self.swap_mut(&mut result, order);
+        drop(result)
+    }
+
+    /// Atomically set this `AtomicOptionBox` to `None` and return the
+    /// previous value.
+    ///
+    /// This does not allocate or free memory, and it neither clones nor drops
+    /// any values. It is equivalent to calling `self.swap(None, order)`
+    ///
+    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
+    /// as other values would not be safe if `T` contains any data.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ordering` is not one of the two allowed values.
+    ///
+    /// # Examples
+    ///
+    ///     use core::sync::atomic::Ordering;
+    ///     use atomicbox::AtomicOptionBox;
+    ///
+    ///     let atom = AtomicOptionBox::new(Some(Box::new("ok")));
+    ///     let prev_value = atom.take(Ordering::AcqRel);
+    ///     assert!(prev_value.is_some());
+    ///     let prev_value = atom.take(Ordering::AcqRel);
+    ///     assert!(prev_value.is_none());
+    ///
+    pub fn take(&self, order: Ordering) -> Option<Box<T>> {
+        self.swap(None, order)
+    }
+
+    /// Atomically swaps the contents of this `AtomicOptionBox` with the contents of `other`.
+    ///
+    /// This does not allocate or free memory, and it neither clones nor drops
+    /// any values. The pointers in `*other` and `self` are simply exchanged.
+    ///
+    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
+    /// as other values would not be safe if `T` contains any data.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ordering` is not one of the two allowed values.
+    ///
+    /// # Examples
+    ///
+    ///     use core::sync::atomic::Ordering;
+    ///     use atomicbox::AtomicOptionBox;
+    ///
+    ///     let atom = AtomicOptionBox::new(None);
+    ///     let mut boxed = Some(Box::new("ok"));
+    ///     let prev_value = atom.swap_mut(&mut boxed, Ordering::AcqRel);
+    ///     assert_eq!(boxed, None);
+    ///
+    pub fn swap_mut(&self, other: &mut Option<Box<T>>, order: Ordering) {
+        match order {
+            Ordering::AcqRel | Ordering::SeqCst => {}
+            _ => panic!("invalid ordering for atomic swap"),
+        }
+
+        let new_ptr = into_ptr(unsafe { ptr::read(other) });
+        let old_ptr = self.ptr.swap(new_ptr, order);
+        unsafe {
+            ptr::write(other, from_ptr(old_ptr));
         }
     }
 
-    /// Create a new `AtomicOptionBox` with the `None` value, useful for static variables.
-    pub const fn none() -> AtomicOptionBox<T> {
-        AtomicOptionBox {
-            inner: AtomicPtr::new(ptr::null_mut()),
-        }
-    }
-
-    /// Takes the value out of the option, leaving a None in its place.
+    /// Consume this `AtomicOptionBox`, returning the last option value it
+    /// contained.
     ///
-    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
-    /// as other values would not be safe if `T` contains any data.
-    pub fn take(&self, ordering: Ordering) -> Option<Box<T>> {
-        self.replace(ptr::null_mut(), ordering)
-    }
-
-    /// Swap the current value with `new`, returning the old value.
+    /// # Examples
     ///
-    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
-    /// as other values would not be safe if `T` contains any data.
-    pub fn swap(&self, new: Option<Box<T>>, ordering: Ordering) -> Option<Box<T>> {
-        let new = new.map_or(ptr::null_mut(), Box::into_raw);
-        self.replace(new, ordering)
-    }
-
-    /// Store a new value.
+    ///     use atomicbox::AtomicOptionBox;
     ///
-    /// `ordering` must be either `Ordering::AcqRel` or `Ordering::SeqCst`,
-    /// as other values would not be safe if `T` contains any data.
-    pub fn store(&self, new: Option<Box<T>>, ordering: Ordering) {
-        let new = new.map_or(ptr::null_mut(), Box::into_raw);
-        self.replace(new, ordering);
+    ///     let atom = AtomicOptionBox::new(Some(Box::new("hello")));
+    ///     assert_eq!(atom.into_inner(), Some(Box::new("hello")));
+    ///
+    pub fn into_inner(self) -> Option<Box<T>> {
+        let last_ptr = self.ptr.load(Ordering::Acquire);
+        forget(self);
+        unsafe { from_ptr(last_ptr) }
     }
 
-    /// Store a new value if and only if the current value is None.
-    pub fn try_store(&self, new: Box<T>, ordering: Ordering) -> bool {
-        let new = Box::into_raw(new);
-        let old = self.inner.compare_and_swap(ptr::null_mut(), new, ordering);
-        // Note, it is not safe to read *old if ordering == Ordering::Relaxed.
-        // However, we do not read the boxed value as old cannot be a box because
-        // of the compare constraint.
-        old.is_null()
-    }
-
-    fn replace(&self, new: *mut T, ordering: Ordering) -> Option<Box<T>> {
-        // It is not safe to read *old if ordering == Ordering::Relaxed
-        // as the Box pointer may not yet be available.
-        assert!(ordering == Ordering::AcqRel || ordering == Ordering::SeqCst);
-        let old = self.inner.swap(new, ordering);
-        if old.is_null() {
+    /// Returns a mutable reference to the contained value.
+    ///
+    /// This is safe because it borrows the `AtomicOptionBox` mutably, which
+    /// ensures that no other threads can concurrently access either the atomic
+    /// pointer field or the boxed data it points to.
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        // I have a convoluted theory that Relaxed is good enough here.
+        // See comment in AtomicBox::get_mut().
+        let ptr = self.ptr.load(Ordering::Relaxed);
+        if ptr.is_null() {
             None
         } else {
-            Some(unsafe { Box::from_raw(old) })
+            Some(unsafe { &mut *ptr })
         }
     }
 }
 
 impl<T> Drop for AtomicOptionBox<T> {
+    /// Dropping an `AtomicOptionBox<T>` drops the final `Box<T>` value (if
+    /// any) stored in it.
     fn drop(&mut self) {
-        let ptr = self.inner.load(Ordering::Acquire);
-        if !ptr.is_null() {
-            unsafe {
-                Box::from_raw(ptr);
+        let ptr = self.ptr.load(Ordering::Acquire);
+        unsafe {
+            from_ptr(ptr);
+        }
+    }
+}
+
+impl<T> Default for AtomicOptionBox<T> {
+    /// The default `AtomicOptionBox<T>` value is `AtomicBox::new(None)`.
+    fn default() -> AtomicOptionBox<T> {
+        AtomicOptionBox::new(None)
+    }
+}
+
+impl<T> Debug for AtomicOptionBox<T> {
+    /// The `{:?}` format of an `AtomicOptionBox<T>` looks like
+    /// `"AtomicOptionBox(0x12341234)"` or `"AtomicOptionBox(None)"`.
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        let p = self.ptr.load(Ordering::Relaxed);
+        f.write_str("AtomicOptionBox(")?;
+        if p.is_null() {
+            f.write_str("None")?;
+        } else {
+            fmt::Pointer::fmt(&p, f)?;
+        }
+        f.write_str(")")?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use core::sync::atomic::Ordering;
+
+    #[test]
+    fn atomic_option_box_swap_works() {
+        let b = AtomicOptionBox::new(Some(Box::new("hello world")));
+        let bis = Box::new("bis");
+        assert_eq!(
+            b.swap(None, Ordering::AcqRel),
+            Some(Box::new("hello world"))
+        );
+        assert_eq!(b.swap(Some(bis), Ordering::AcqRel), None);
+        assert_eq!(b.swap(None, Ordering::AcqRel), Some(Box::new("bis")));
+    }
+
+    #[test]
+    fn atomic_option_box_store_works() {
+        let b = AtomicOptionBox::new(Some(Box::new("hello world")));
+        b.store(None, Ordering::AcqRel);
+        assert_eq!(b.into_inner(), None);
+
+        let b = AtomicOptionBox::new(Some(Box::new("hello world")));
+        let bis = Box::new("bis");
+        b.store(Some(bis), Ordering::AcqRel);
+        assert_eq!(b.into_inner(), Some(Box::new("bis")));
+    }
+
+    #[test]
+    fn atomic_option_box_swap_mut_works() {
+        let b = AtomicOptionBox::new(Some(Box::new("hello world")));
+        let mut bis = None;
+        b.swap_mut(&mut bis, Ordering::AcqRel);
+        assert_eq!(bis, Some(Box::new("hello world")));
+        bis = Some(Box::new("bis"));
+        b.swap_mut(&mut bis, Ordering::AcqRel);
+        assert_eq!(bis, None);
+        b.swap_mut(&mut bis, Ordering::AcqRel);
+        assert_eq!(bis, Some(Box::new("bis")));
+    }
+
+    #[test]
+    fn atomic_option_box_pointer_identity() {
+        let box1 = Box::new(1);
+        let p1 = format!("{:p}", box1);
+        let atom = AtomicOptionBox::new(Some(box1));
+
+        let box2 = Box::new(2);
+        let p2 = format!("{:p}", box2);
+        assert!(p2 != p1);
+
+        let box3 = atom.swap(Some(box2), Ordering::AcqRel).unwrap(); // box1 out, box2 in
+        let p3 = format!("{:p}", box3);
+        assert_eq!(p3, p1); // box3 is box1
+
+        let box4 = atom.swap(None, Ordering::AcqRel).unwrap(); // box2 out, None in
+        let p4 = format!("{:p}", box4);
+        assert_eq!(p4, p2); // box4 is box2
+    }
+
+    #[test]
+    fn atomic_box_drops() {
+        use core::sync::atomic::{AtomicUsize, Ordering};
+        use core::sync::Arc;
+
+        struct K(Arc<AtomicUsize>, usize);
+
+        impl Drop for K {
+            fn drop(&mut self) {
+                self.0.fetch_add(self.1, Ordering::Relaxed);
             }
         }
+
+        let n = Arc::new(AtomicUsize::new(0));
+        {
+            let ab = AtomicOptionBox::new(Some(Box::new(K(n.clone(), 5))));
+            assert_eq!(n.load(Ordering::Relaxed), 0);
+            let first = ab.swap(None, Ordering::AcqRel);
+            assert_eq!(n.load(Ordering::Relaxed), 0);
+            drop(first);
+            assert_eq!(n.load(Ordering::Relaxed), 5);
+            let second = ab.swap(Some(Box::new(K(n.clone(), 13))), Ordering::AcqRel);
+            assert_eq!(second.is_none(), true);
+            assert_eq!(n.load(Ordering::Relaxed), 5);
+        }
+        assert_eq!(n.load(Ordering::Relaxed), 5 + 13);
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid ordering for atomic swap")]
+    fn cant_use_foolish_swap_ordering_type() {
+        let atom = AtomicOptionBox::new(Some(Box::new(0)));
+        atom.swap(None, Ordering::Release); // nope
+    }
+
+    #[test]
+    fn debug_fmt() {
+        let my_box = Box::new(32);
+        let expected = format!("AtomicOptionBox({:p})", my_box);
+        assert_eq!(
+            format!("{:?}", AtomicOptionBox::new(Some(my_box))),
+            expected
+        );
+        assert_eq!(
+            format!("{:?}", AtomicOptionBox::<String>::new(None)),
+            "AtomicOptionBox(None)"
+        );
     }
 }
