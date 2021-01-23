19d94716e509e0ff0f8241fde46ede52a1a5f15c
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-01-23 08:46:17 +0100

    Make sure that systick timeout is stopped if the handle is dropped

--- src/drivers/systick/alarm.rs
+++ src/drivers/systick/alarm.rs
@@ -1,7 +1,10 @@
+use core::{pin::Pin, task::{Context, Poll}};
+
 use crate::{AlarmCounter, AlarmTimer, AlarmTimerMode, Tick, drivers::cortexm::burn};
 use async_trait::async_trait;
 use drone_core::{fib, thr::prelude::*};
-use drone_cortexm::{map::{periph::sys_tick::SysTickPeriph}, reg::prelude::*};
+use drone_cortexm::{map::{periph::sys_tick::SysTickPeriph, reg::stk}, reg::prelude::*};
+use futures::Future;
 
 use super::{diverged::SysTickDiverged, Adapter};
 
@@ -25,6 +28,48 @@ impl<Int: ThrToken> SysTickAlarmDrv<Int> {
     }
 }
 
+impl<Int: ThrToken> SysTickTimerDrv<Int> {
+    fn delay_impl(&mut self, duration: u32) -> DelayFuture {
+        let stk_ctrl = self.0.stk_ctrl;
+        let future = Box::pin(self.1.add_future(fib::new_fn(move || {
+            if stk_ctrl.countflag.read_bit() {
+                stk_ctrl.modify(|r| r.clear_tickint().clear_enable());
+
+                fib::Complete(())
+            } else {
+                fib::Yielded(())
+            }
+        })));
+
+        self.0.stk_load.store(|r| r.write_reload(duration));
+        self.0.stk_ctrl.modify(|r| r.set_tickint().set_enable());
+
+        DelayFuture {
+            stk_ctrl,
+            future,
+        }
+    }
+}
+
+struct DelayFuture<'a> {
+    stk_ctrl: stk::Ctrl<Crt>,
+    future: Pin<Box<dyn Future<Output = ()> + Send + 'a>>,
+}
+
+impl Future for DelayFuture<'_> {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        self.future.as_mut().poll(cx)
+    }
+}
+
+impl Drop for DelayFuture<'_> {
+    fn drop(&mut self) {
+        self.stk_ctrl.modify(|r| r.clear_tickint().clear_enable());
+    }
+}
+
 impl<T: Tick> AlarmCounter<T, Adapter> for SysTickCounterDrv {
     fn value(&self) -> u32 {
         0 // The counter is not running
@@ -41,20 +86,6 @@ impl<Int: ThrToken, T: Tick + 'static> AlarmTimer<T, Adapter> for SysTickTimerDr
     const MODE: AlarmTimerMode = AlarmTimerMode::OneShotOnly;
 
     async fn delay(&mut self, duration: u32) {
-        let stk_ctrl = self.0.stk_ctrl;
-        let timeout_future = Box::pin(self.1.add_future(fib::new_fn(move || {
-            if stk_ctrl.countflag.read_bit() {
-                stk_ctrl.modify(|r| r.clear_tickint().clear_enable());
-
-                fib::Complete(())
-            } else {
-                fib::Yielded(())
-            }
-        })));
-
-        self.0.stk_load.store(|r| r.write_reload(duration));
-        self.0.stk_ctrl.modify(|r| r.set_tickint().set_enable());
-
-        timeout_future.await;
+        self.delay_impl(duration).await;
     }
 }
